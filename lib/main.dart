2514871e8a33b91615e9af336b58733456da91c0
Paung <paungcuy15@gmail.com>
2024-09-10 10:58:03 +0700

    dh tuh

--- lib/main.dart
+++ lib/main.dart
@@ -7,50 +7,78 @@ import './Detail.dart';
 import './adddata.dart';
 
 void main() {
-  runApp(new MaterialApp(
+  runApp(MaterialApp(
     title: "My Store",
-    home: new Home(),
+    home: Home(),
   ));
 }
 
 class Home extends StatefulWidget {
   @override
-  _HomeState createState() => new _HomeState();
+  _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  late Future<List> _futureData;
+
+  @override
+  void initState() {
+    super.initState();
+    _futureData = getData();
+  }
+
   Future<List> getData() async {
     final response = await http.get(Uri.parse("http://192.168.56.1/crud-flutter-api/getdata.php"));
     return json.decode(response.body);
   }
 
+  void _refreshData() {
+    setState(() {
+      _futureData = getData();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("MY STORE"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("CRUD Flutter"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: _refreshData,
+          ),
+        ],
       ),
-      floatingActionButton: new FloatingActionButton(
-        child: new Icon(Icons.add),
-        onPressed: ()=>Navigator.of(context).push(
-          new MaterialPageRoute(
-            builder: (BuildContext context)=> new AddData(),
-          )
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (BuildContext context) => AddData(),
+          ),
         ),
       ),
-      body: new FutureBuilder<List>(
-        future: getData(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) print(snapshot.error);
-
-          return snapshot.hasData
-              ? new ItemList(
-                  list: snapshot.data??[],
-                )
-              : new Center(
-                  child: new CircularProgressIndicator(),
-                );
+      body: RefreshIndicator(
+        notificationPredicate: true ? (_) => true : (_) => false,
+        onRefresh: () async {
+          _refreshData();
+          // Return a Future to complete the refresh
+          await _futureData;
         },
+        child: FutureBuilder<List>(
+          future: _futureData,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) print(snapshot.error);
+
+            return snapshot.hasData
+                ? ItemList(
+                    list: snapshot.data ?? [],
+                  )
+                : Center(
+                    child: CircularProgressIndicator(),
+                  );
+          },
+        ),
       ),
     );
   }
@@ -62,23 +90,22 @@ class ItemList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new ListView.builder(
-      itemCount: list == null ? 0 : list.length,
+    return ListView.builder(
+      itemCount: list.length,
       itemBuilder: (context, i) {
-        return new Container(
+        return Container(
           padding: const EdgeInsets.all(10.0),
-          child: new GestureDetector(
-            onTap: ()=>Navigator.of(context).push(
-              new MaterialPageRoute(
-                builder: (BuildContext context)=> new Detail(list:list , index: i,)
-              )
+          child: GestureDetector(
+            onTap: () => Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (BuildContext context) => Detail(list: list, index: i),
+              ),
             ),
-            child: new Card(
-
-              child: new ListTile(
-                title: new Text(list[i]['item_name']),
-                leading: new Icon(Icons.widgets),
-                subtitle: new Text("Stock : ${list[i]['stock']}"),
+            child: Card(
+              child: ListTile(
+                title: Text(list[i]['item_name']),
+                leading: Icon(Icons.widgets),
+                subtitle: Text("Stock : ${list[i]['stock']}"),
               ),
             ),
           ),
